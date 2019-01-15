a2806e5faf74dc2a36b0fa05058879303503b183
Thiago Cunha <grupocodenome@gmail.com>
2019-01-15 15:58:40 -0200

    Atualizando scripts

---
+++ PersonApi/bin/Debug/netcoreapp2.0/db/dataset/V1_0_10__Insert_data_in_users.sql
@@ -0,0 +1,3 @@
+INSERT INTO `users` (`login`, `AccessKey`) VALUES ('thiago', '123123');
+INSERT INTO `users` (`login`, `AccessKey`) VALUES ('thalita', '123123');
+INSERT INTO `users` (`login`, `AccessKey`) VALUES ('carol', '123123');
